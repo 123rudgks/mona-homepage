06eea958484e47db5a834ca3130c8fe243c9374a
KimGyeongHan <123rudgks@gmail.com>
2024-10-14 20:37:44 +0900

    WIP : Main > Section3 Card border에만 gradient 주기 어려움 디자인과 다르지만 비슷한 모양새로 수정함

--- components/pages/Main/TopRightRoundedCard.tsx
+++ components/pages/Main/TopRightRoundedCard.tsx
@@ -12,17 +12,17 @@ const TopRightRoundedCard = ({ theme }: Props) => {
         'lg-screen:max-w-[460px]',
         'sm-screen:min-h-[565px] sm-screen:rounded-3xl sm-screen:rounded-tr-[124px]',
         'min-h-[260px]',
-        'p-[1px] overflow-hidden',
-        'bg-gradient-to-br from-whiteAlpha-70 to-transparent',
+        'p-[1px] overflow-hidden ',
+        'bg-gradient-to-br from-whiteAlpha-10 to-transparent',
       )}>
       <div
         className={cn(
           'rounded-xl rounded-tr-[60px]',
-          'sm-screen:rounded-3xl sm-screen:rounded-tr-[124px]',
-          'w-full h-full backdrop-blur-[50px] ',
+          'sm-screen:rounded-3xl sm-screen:rounded-tr-[124px] ',
+          'w-full h-full',
           theme === 'primary'
             ? ' bg-gradient-to-b from-primary to-primary-alpha-50 '
-            : 'bg-whiteAlpha-10',
+            : 'bg-gradient-to-br  from-blackAlpha-50 to-blackAlpha-10',
         )}></div>
     </div>
   );
