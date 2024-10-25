78fee3a7f444214736ddc2f6a0d7e766b7f9534d
KimGyeongHan <123rudgks@gmail.com>
2024-10-25 15:13:23 +0900

    feat : Main > Section6 NewsItem content line-clamp 처리

--- components/pages/Main/NewsItem.tsx
+++ components/pages/Main/NewsItem.tsx
@@ -21,7 +21,7 @@ export const DUMMY_NEWS: Array<{
     id: 1,
     title: '한국IBM "생성형 AI로 더 복잡해진 IT, 자동화는 필수',
     content:
-      '“오늘날 기업들은 다양한 클라우드 환경과 수 많은 애플리케이션을 비즈니스에서 활용하고 있으며, 이 환경은 생성형 AI의 도입으로 더욱 복잡해지고 있다. 생성형 AI가 2028년까지 최대 10억 개의 앱을 만들어낼 것으로 예상되는 상황에서 자동화는 더 이상 선택 사항이 아니다.” 이은주 한국IBM 사장은 13일 서울 여의도 담회...',
+      '오늘날 기업들은 다양한 클라우드 환경과 수 많은 애플리케이션을 비즈니스에서 활용하고 있으며, 이 환경은 생성형 AI의 도입으로 더욱 복잡해지고 있다. 생성형 AI가 2028년까지 최대 10억 개의 앱을 만들어낼 것으로 예상되는 상황에서 자동화는 더 이상 선택 사항이 아니다.” 이은주 한국IBM 사장은 13일 서울 여의도 담회 오늘날 기업들은 다양한 클라우드 환경과 수 많은 애플리케이션을 비즈니스에서 활용하고 있으며, 이 환경은 생성형 AI의 도입으로 더욱 복잡해지고 있다. 생성형 AI가 2028년까지 최대 10억 개의 앱을 만들어낼 것으로 예상되는 상황에서 자동화는 더 이상 선택 사항이 아니다.” 이은주 한국IBM 사장은 13일 서울 여의도 담회 오늘날 기업들은 다양한 클라우드 환경과 수 많은 애플리케이션을 비즈니스에서 활용하고 있으며, 이 환경은 생성형 AI의 도입으로 더욱 복잡해지고 있다. 생성형 AI가 2028년까지 최대 10억 개의 앱을 만들어낼 것으로 예상되는 상황에서 자동화는 더 이상 선택 사항이 아니다.” 이은주 한국IBM 사장은 13일 서울 여의도 담회 ',
     thumbnail: 'https://picsum.photos/200',
     date: new Date('2024-06-30'),
   },
@@ -56,7 +56,7 @@ const NewsItem = ({ id, title, content, thumbnail, date }: Props) => {
     <div
       className={cn(
         'flex flex-col gap-4',
-        'sm-screen:flex-row sm-screen:gap-8 sm-screen:h-[203px] h-[302px]',
+        'sm-screen:flex-row sm-screen:gap-8 sm-screen:h-[203px] min-h-[302px]',
       )}>
       <div className="w-full h-[184px] shrink-0 sm-screen:w-[363px] sm-screen:h-full relative rounded-3xl overflow-hidden">
         <Image src={thumbnail} alt={title} fill />
@@ -69,7 +69,7 @@ const NewsItem = ({ id, title, content, thumbnail, date }: Props) => {
           className={cn(
             'flex-1 sm-screen:typo-TitleMedium typo-BodySmallMedium text-blackAlpha-50 flex flex-col justify-between',
           )}>
-          <div>{content}</div>
+          <div className="sm-screen:line-clamp-4 line-clamp-2">{content}</div>
           <div>{dayjs(date).format('YYYY.MM.DD')}</div>
         </div>
       </div>
