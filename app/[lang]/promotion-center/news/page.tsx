5772a1e689929b8709a150c102207375dd2883b8
KimGyeongHan <123rudgks@gmail.com>
2024-11-22 00:49:53 +0900

    feat : News Page UI done

--- app/[lang]/promotion-center/news/page.tsx
+++ app/[lang]/promotion-center/news/page.tsx
@@ -1,7 +1,239 @@
+'use client';
+import InputCancel from '@/app/svgs/promotion-center/news/InputCancel.svg';
+import InputSearchIcon from '@/app/svgs/promotion-center/news/InputSearch.svg';
+import BoardSection from '@/components/BoardSection';
+import ContentBox from '@/components/ContentBox';
+import ContentSection from '@/components/ContentSection';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import MonaBreadCrumb from '@/components/MonaBreadCrumb';
+import { MobileTabMenu } from '@/components/TabMenu';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
+import useMenu from '@/hooks/useMenu';
+import { cn } from '@/lib/utils';
+import { Language } from '@/types/globals.types';
+import { HomeIcon } from 'lucide-react';
+import Image from 'next/image';
+import { useState } from 'react';
+
 type Props = {};
 
-const Page = (props: Props) => {
-  return <div>Page</div>;
+const Page = ({ params: { lang } }: { params: { lang: Language } }) => {
+  const { MENU, currentCategory, currentMenu } = useMenu({ lang });
+
+  return (
+    <main>
+      <BoardSection
+        title={currentCategory?.category}
+        desc={['RAPID & ACCURATE BATTERY DIAGNOSIS', 'Powered By AI']}
+      />
+      <ContentSection mobileTabMenuComp={<MobileTabMenu lang={lang} />}>
+        <div
+          className={cn(
+            'lg-screen:h-[100px] sm-screen:h-20 h-[60px] flex items-center justify-end',
+          )}>
+          <MonaBreadCrumb
+            items={[
+              { href: '/', component: <HomeIcon />, id: 'home' },
+
+              {
+                component: currentCategory?.category,
+                id: 'category',
+              },
+            ]}
+          />
+        </div>
+        <div
+          className={cn(
+            'sm-screen:flex lg-screen:flex-row lg-screen:gap-11 sm-screen:flex-col sm-screen:gap-9',
+          )}>
+          <ContentBox
+            title={currentMenu.text}
+            subTitle={
+              <InputSearch
+                placeholder="Search keyword"
+                className={cn(
+                  'mt-2 sm-screen:mt-0 sm-screen:absolute  sm-screen:right-0 sm-screen:bottom-0',
+                )}
+              />
+            }
+            label={currentMenu.label}>
+            <div
+              className={cn(
+                'grid grid-cols-1 gap-7 mb-[30px]',
+                'sm-screen:grid-cols-2 sm-screen:gap-6 sm-screen:mb-9',
+                'lg-screen:grid-cols-3 lg-screen:gap-10',
+              )}>
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />{' '}
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+              <NewsCard
+                img="https://picsum.photos/seed/picsum/200/300"
+                title="더블디, 신규 매장 오픈 더블디, 신규 매장 오픈 더블디, 신규 매장 오픈"
+                date="2022-06-18"
+              />
+            </div>
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <PaginationPrevious></PaginationPrevious>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#" isActive>
+                    1
+                  </PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationLink href="#">2</PaginationLink>
+                </PaginationItem>
+                <PaginationItem>
+                  <PaginationNext></PaginationNext>
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </ContentBox>
+        </div>
+      </ContentSection>
+      <Header lang={lang} />
+      <Footer lang={lang} />
+    </main>
+  );
+};
+
+const NewsCard = ({
+  img,
+  title,
+  date,
+}: {
+  img: string;
+  title: string;
+  date: string;
+}) => {
+  return (
+    <div className={cn('w-full flex flex-col gap-3', 'gap-5')}>
+      <div className={cn('relative w-full h-[327px]', 'sm-screen:h-[450px]')}>
+        <Image src={img} alt={title} fill />
+      </div>
+      <div className={cn('flex flex-col gap-2', 'gap-3')}>
+        <div
+          className={cn(
+            'text-grayscale-700',
+            'typo-HeadlineBold',
+            'sm-screen:typo-Display1Bold',
+          )}>
+          {title}
+        </div>
+        <div className={cn('text-blackAlpha-70', 'typo-BodyLargeRegular')}>
+          {date}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const InputSearch = ({
+  className,
+  onCancel,
+  onSearch,
+  ...props
+}: React.ComponentProps<'input'> & {
+  onCancel?: () => void;
+  onSearch?: () => void;
+}) => {
+  const [focused, setFocused] = useState(false);
+  const [filled, setFilled] = useState(false);
+  return (
+    <div
+      className={cn(
+        'sm-screen:w-[450px] w-full h-10 p-2 border border-grayscale-200 rounded-[4px] flex',
+        focused && props.disabled
+          ? 'ring-[3px] ring-[#97979739] border-grayscale-black'
+          : '',
+        props.disabled ? 'border-grayscale-200 bg-grayscale-50' : '',
+        className,
+      )}>
+      <input
+        className="w-full h-full outline-none typo-BodyLargeRegular placeholder:text-grayscale-400 bg-transparent"
+        {...props}
+        onFocus={(e) => {
+          setFocused(true);
+          props.onFocus?.(e);
+        }}
+        onBlur={(e) => {
+          setFocused(false);
+          props.onBlur?.(e);
+        }}
+        onChange={(e) => {
+          setFilled(!!e.target.value);
+          props.onChange?.(e);
+        }}
+      />
+      <div className="flex gap-[6px] items-center">
+        {filled && !props.disabled && (
+          <InputCancel
+            className="cursor-pointer"
+            onClick={() => {
+              onCancel?.();
+              setFilled(false);
+            }}
+          />
+        )}
+        <InputSearchIcon
+          className={cn(
+            'cursor-pointer',
+            props.disabled ? '[&>path]:fill-grayscale-300' : '',
+          )}
+          onClick={() => {
+            onSearch?.();
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ components/ui/pagination.tsx
@@ -0,0 +1,117 @@
+import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
+  <nav
+    role="navigation"
+    aria-label="pagination"
+    className={cn('mx-auto flex w-full justify-center', className)}
+    {...props}
+  />
+);
+Pagination.displayName = 'Pagination';
+
+const PaginationContent = React.forwardRef<
+  HTMLUListElement,
+  React.ComponentProps<'ul'>
+>(({ className, ...props }, ref) => (
+  <ul
+    ref={ref}
+    className={cn('flex flex-row items-center gap-1', className)}
+    {...props}
+  />
+));
+PaginationContent.displayName = 'PaginationContent';
+
+const PaginationItem = React.forwardRef<
+  HTMLLIElement,
+  React.ComponentProps<'li'>
+>(({ className, ...props }, ref) => (
+  <li ref={ref} className={cn('', className)} {...props} />
+));
+PaginationItem.displayName = 'PaginationItem';
+
+type PaginationLinkProps = {
+  isActive?: boolean;
+} & React.ComponentProps<'a'>;
+
+const PaginationLink = ({
+  className,
+  isActive,
+  ...props
+}: PaginationLinkProps) => (
+  <a
+    aria-current={isActive ? 'page' : undefined}
+    className={cn(
+      'py-[6px] px-[14px] rounded-full flex justify-center items-center typo-BodyLargeBold',
+      isActive
+        ? 'bg-primary text-white'
+        : 'text-grayscale-500 hover:bg-grayscale-50',
+      className,
+    )}
+    {...props}
+  />
+);
+PaginationLink.displayName = 'PaginationLink';
+
+const PaginationPrevious = ({
+  className,
+  disabled,
+  ...props
+}: React.ComponentProps<'a'> & {
+  disabled?: boolean;
+}) => (
+  <a className={cn(disabled ? '' : 'cursor-pointer', className)} {...props}>
+    <ChevronLeft
+      className={cn(
+        'h-8 w-8 ',
+        disabled ? 'stroke-grayscale-300' : 'stroke-grayscale-400',
+      )}
+    />
+  </a>
+);
+PaginationPrevious.displayName = 'PaginationPrevious';
+
+const PaginationNext = ({
+  className,
+  disabled,
+  ...props
+}: React.ComponentProps<'a'> & {
+  disabled?: boolean;
+}) => (
+  <a className={cn(disabled ? '' : 'cursor-pointer', className)} {...props}>
+    <ChevronRight
+      className={cn(
+        'h-8 w-8 ',
+        disabled ? 'stroke-grayscale-300' : 'stroke-grayscale-400',
+      )}
+    />
+  </a>
+);
+PaginationNext.displayName = 'PaginationNext';
+
+const PaginationEllipsis = ({
+  className,
+  ...props
+}: React.ComponentProps<'span'>) => (
+  <span
+    aria-hidden
+    className={cn('flex h-9 w-9 items-center justify-center', className)}
+    {...props}>
+    <MoreHorizontal className="h-4 w-4" />
+    <span className="sr-only">More pages</span>
+  </span>
+);
+PaginationEllipsis.displayName = 'PaginationEllipsis';
+
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+};
